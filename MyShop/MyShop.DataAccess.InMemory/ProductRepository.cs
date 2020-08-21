cff0f905ab754fc7ee935dbeae6f88c8382b169a
Eze Philip <59514457+philo94@users.noreply.github.com>
2020-08-21 04:03:27 +0200

    ProductModel and InMemroy Product Repository Built

---
+++ MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//add using statements
+using System.Runtime.Caching;
+using Myshop.Core.Models;
+
+namespace MyShop.DataAccess.InMemory
+{
+   public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products;
+
+        public ProductRepository()
+        {
+            // check to see if a cache "Products" exist, else create the cache
+            products = cache["products"] as List<Product>;
+            if (products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+
+        //Method that persist a product instead of adding 
+        //into the list until necessary
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        //Method to insert into Product List
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+
+        //Method to Update the Product List
+        public void Update(Product product)
+        {
+            //Look into database to find product to be updated
+            Product productToUpdate = products.Find(p => p.Id == product.Id);
+
+            if (productToUpdate != null)
+            {
+                productToUpdate = product;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+
+        //Method to find a product in a database
+        public Product Find(String Id)
+        {
+            Product product = products.Find(p => p.Id ==Id);
+
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+
+        }
+
+        //Method to Return list of products
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+
+
+        //Method to delete Product from List
+        public void Delete(string Id)
+        {
+            Product productToDelete = products.Find(p => p.Id == Id);
+
+            if (productToDelete != null)
+            {
+                products.Remove(productToDelete);
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+    }
+}
---
+++ MyShop/Myshop.Core/Models/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myshop.Core.Models
+{
+   // public class to enable other programs and projects access its properties
+    public class Product
+    {
+        public string Id { get; set; }
+
+        //Data validation
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        //Data validation
+        [Range(0, 1000)]
+        public decimal Price { get; set; }
+
+        //Group products into categories
+        public string Category { get; set; }
+
+        //contains URL for our product Image
+        public string Image { get; set; }
+
+        public Product()
+        {
+            //Id is created as string but generated as GUID
+            this.Id = Guid.NewGuid().ToString();
+        }
+        
+    }
+}
