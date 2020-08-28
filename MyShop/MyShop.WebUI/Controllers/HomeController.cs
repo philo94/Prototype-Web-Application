161ade74c93ef969317d04398986d90aa1189bbb
Eze Philip <59514457+philo94@users.noreply.github.com>
2020-08-28 06:48:16 +0200

    product Listing completed

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Myshop.Core.Models;
+using MyShop.Core.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,41 @@ namespace MyShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        //Creating an instance of IRepository
+        IRepository<Product> context;
+
+        // Add the repository so that we can load the product categories from the database
+        IRepository<ProductCategory> ProductCategories;
+
+        //Initialize the product category
+
+
+        //constructor that initializes the repository
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            context = productContext;
+            //Initialize the product category
+            ProductCategories = productCategoryContext;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        //method that creates the product page
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+            if(product == null)
+            {
+                return HttpNotFound();
+
+            }
+            else
+            {
+                return View(product);
+            }
         }
 
         public ActionResult About()
