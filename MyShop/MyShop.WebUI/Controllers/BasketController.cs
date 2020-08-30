d579a7800d2026ab9643b359bbf7ec3246ab106c
Eze Philip <59514457+philo94@users.noreply.github.com>
2020-08-30 06:38:28 +0200

    Customers linked to Orders

--- MyShop/MyShop.WebUI/Controllers/BasketController.cs
+++ MyShop/MyShop.WebUI/Controllers/BasketController.cs
@@ -1,5 +1,6 @@
 ﻿using Myshop.Core.Contracts;
 using Myshop.Core.Models;
+using MyShop.Core.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,13 +12,15 @@ namespace MyShop.WebUI.Controllers
 {
     public class BasketController : Controller
     {
+        IRepository<Customer> customers;
         IBasketService basketService;
         IOrderService orderService;
 
-        public BasketController(IBasketService BasketService, IOrderService OrderService)
+        public BasketController(IBasketService BasketService, IOrderService OrderService, IRepository<Customer> Customers)
         {
             this.basketService = BasketService;
             this.orderService = OrderService;
+            this.customers = Customers;
         }
 
         // GET: Basket
@@ -48,17 +51,44 @@ namespace MyShop.WebUI.Controllers
         }
 
         //Endpoint that returns a check-out page
+        // Links checkout to Authorised User
+        [Authorize]
         public ActionResult Checkout()
         {
-            return View();
+            Customer customer = customers.Collection().FirstOrDefault(c => c.Email == User.Identity.Name);
+            if (customer != null)
+            {
+                Order order = new Order()
+                {
+                    Email = customer.Email,
+                    City = customer.City,
+                    State = customer.State,
+                    Street = customer.Street,
+                    FirstName = customer.FirstName,
+                    SurName = customer.LastName,
+                    ZipCode = customer.ZipCode
+
+                };
+                return View(order);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+           
         }
 
         //Check out method that processes the order
+        // Authorize is used to prevent a fradulent Request
+        // This is done by linking the customers email to the Checkout method 
+        // to ensure that the checkout is firmly connected with the right customer
         [HttpPost]
+        [Authorize]
         public ActionResult Checkout(Order order)
         {
             var basketItems = basketService.GetBasketItems(this.HttpContext);
             order.OrderStatus = "Order Created";
+            order.Email = User.Identity.Name;
 
             //process the payment
 
